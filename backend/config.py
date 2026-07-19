9705bc58c89d6f244b201b9c694baca7912afeb2
Rakshitha K N <154422751+Rakshitha-arya@users.noreply.github.com>
2026-07-19 21:16:31 +0530

    Add default values for configuration settings
    Set default values for SECRET_KEY, DATABASE_URL, and JWT_SECRET_KEY in the Config class.

--- backend/config.py
+++ backend/config.py
@@ -6,13 +6,19 @@ load_dotenv()
 
 class Config:
 
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///recruitment.db"
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv(
+        "JWT_SECRET_KEY",
+        "your_jwt_secret_key"
+    )
 
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
 
